8b91715af9d5957665e9fe4aa427aeb2aa88d16b
Mahesh Kumar <maheshkumar.wdp21@gmail.com>
2014-10-22 22:26:28 +0530

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,11 +1,14 @@
+// Program to sort the given set of elements using Insertion Sort.
+// Logic of Insertion Sort, a key element will be compared with all the elements before it and sorting will happen accordingly.
+// Complexity Of Insertion Sort
+// Best case -> O(n)
+// Worst case -> O(n^2)
 #include<conio.h>
 #include<stdio.h>
 
-int swap(int, int); // function to swap two elements
-
 void main()
 {
-	int a[],i,j,n;
+	int a[],i,j,n,key;
 	printf("Enter the value of n:\t");
 	scanf("%d",&n);
 	printf("Enter %d values into the array\n",n);
@@ -13,18 +16,16 @@ void main()
 	{
 		scanf("%d",&a[i]);
 	} 
-	for(i=1;i<n;i++)               // for loops to compare two elements
+	for(i=1;i<n;i++)
 	{
-		for{j=1;j<i;j++)
-			{
-				while(j>0 && (a[j-1] > a[j]))
-				{
-					swap(a[j-1],a[j]);	
-				}
-				j = j-1;
-			}
+		key = a[i];
+		j = j-1;
+		while(j>=0 && (key<a[j]))
+		{
+			a[j+1]=a[j];
+			j--;
+		}a[j+1]=key;
 	}
-	
 	for(i=0;i<n;i++)
 	{
 		printf("After sorting\n");
@@ -32,11 +33,3 @@ void main()
 	}
 }
 
-int swap(int x, int y)     // function swapping two elements and returning the final value back to the main function.
-{
-	int temp;
-	temp = x;
-	x = y;
-	y = temp;
-	return x,y;
-}

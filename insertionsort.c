15ce9404a07cbb4200c3e84cb9da7441962d8632
Mahesh Kumar K <maheshkumar.wdp21@gmail.com>
2014-10-24 19:33:55 +0530

    Modified insertion sort

--- insertionsort.c
+++ insertionsort.c
@@ -1,41 +1,53 @@
-#include<conio.h>
+/* Program to sort an array of elements using Insertion Sort.
+ * Using Insertion Sort, an element will be compared with all the elements before it.
+ * Consider an unsorted array of elements, 3,4,0,5,1.
+ * Now, consider 0, when i = 2, this means that the element 0 will be compared with the elements before it and they are 3 and 4 respectively.
+*/
+
+/* Complexity of Insertion Sort.
+ * Best Case - O(n)
+ * Worst Case - O(n^2)
+*/
+
+// Author : Mahesh Kumar K
+
+
 #include<stdio.h>
-
-int swap(int, int); // function to swap two elements
-
 void main()
 {
-	int a[],i,j,n;
-	printf("Enter the value of n:\t");
-	scanf("%d",&n);
-	printf("Enter %d values into the array\n",n);
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	} 
-	for(i=0;i<n;i++)
-	{
-		for{j=0;j<i;j++)
-			{
-				while(j>0 && (a[j-1] > a[j]))
-				{
-					swap(a[j-1],a[j]);	
-				}
-				j = j-1;
-			}
-	}
-	for(i=0;i<n;i++)
-	{
-		printf("After sorting\n");
-		printf(a[i]);
-	}
+    long a[],n;
+    int i,key;
+    printf("Enter the value of n:\t");
+    scanf("%d",&n);
+    printf("Enter %d values into the array\n",n);
+    for(i=0;i<n;i++)
+        {
+        scanf("%d",&a[i]);
+        }
+
+    printf("Before sorting\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\t",a[i]);
+    }
+
+    for(i=1;i<n;i++)
+    {
+        key = a[i];
+        while(key>=0 && a[key-1]<a[key])
+            {
+            int temp = a[key];
+            a[key]= a[key-1];
+            a[key-1]=a[key];
+            key--;
+            }
+    }
+
+    printf("After sorting\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\t",a[i]);
+    }
 }
 
-int swap(int x, int y)
-{
-	int temp;
-	temp = x;
-	x = y;
-	y = temp;
-	return x,y;
-}
+
